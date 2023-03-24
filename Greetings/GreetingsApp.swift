efd1b749b2873a84ac320bf26d5bc61bdae48f23
Maciej Plewko <maciej.plewko@gmail.com>
2023-03-25 00:35:02 +0100

    Initial iOS project

---
+++ Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by Maciej Plewko on 25/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
